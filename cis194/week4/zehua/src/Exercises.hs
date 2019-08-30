1c9484e8a67a13482e12abfa05646bf3daaff5fb
Zehua Liu <zehua.liu@gmail.com>
2019-08-30 23:06:15 +1000

    week4 ex 2,3,4

--- cis194/week4/zehua/src/Exercises.hs
+++ cis194/week4/zehua/src/Exercises.hs
@@ -1,6 +1,7 @@
 module Exercises where
 
-import Data.Bool (bool)
+import           Data.Bool (bool)
+import           Data.List (group, sort)
 
 -- ex1
 fun1 :: [Integer] -> Integer
@@ -25,3 +26,61 @@ fun2' = foldr (+) 0
     where
       oddv = (+1) . (*3)
       evenv = (`div` 2)
+
+
+-- ex2
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a)
+  deriving (Show, Eq)
+
+getHeight :: Tree a -> Integer
+getHeight Leaf = -1
+getHeight (Node h1 _ _ _) = h1
+
+foldTree :: [a] -> Tree a
+foldTree = foldr f Leaf
+  where
+    f x Leaf = Node 0 Leaf x Leaf
+    f x (Node h l n r)
+        | hl == hr  = let l'  = f x l
+                          hl' = getHeight l'
+                          h'  = bool h (h+1) (hl < hl')
+                      in Node h' l' n r
+        | hl < hr   = Node h (f x l) n r
+        | otherwise = Node h l n (f x r)
+      where
+        hl = getHeight l
+        hr = getHeight r
+
+
+-- ex3
+xor2 :: Bool -> Bool -> Bool
+xor2 = (/=)
+
+xor :: [Bool] -> Bool
+xor = foldr xor2 False
+
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr ((:) . f) []
+
+
+myFoldl :: (a -> b -> a) -> a -> [b] -> a
+myFoldl f base = foldr (flip f) base . reverse
+
+
+-- ex4
+cartProd :: [a] -> [b] -> [(a, b)]
+cartProd xs ys = [(x,y) | x <- xs, y <- ys]
+
+sieveSundaram :: Integer -> [Integer]
+sieveSundaram n = map ((+1) . (*2))
+                . concat
+                . filter ((==1) . length)
+                . group
+                . sort
+                . ([1..n] ++)
+                . filter (<=n)
+                . map (\(i, j) -> i + j + 2 * i * j)
+                . filter (uncurry (<=))
+                $ cartProd [1..n] [1..n]
--- cis194/week4/zehua/test/ExercisesSpec.hs
+++ cis194/week4/zehua/test/ExercisesSpec.hs
@@ -2,8 +2,28 @@ module ExercisesSpec where
 
 -- import           Control.Applicative (liftA2)
 import           Control.Monad (liftM2)
+import           Data.Bool (bool)
+import           Data.Char (isSpace, isUpper)
+import           Data.Numbers.Primes (primes)
 import           Exercises
 import           Test.Hspec
+
+isBalancedTree :: Tree a -> Bool
+isBalancedTree Leaf = True
+isBalancedTree (Node h l _ r) =
+    bool (h == hl + 1 && hl - hr <= 1) (h == hr + 1 && hr - hl <= 1) (hl < hr)
+  where
+    hl = getHeight l
+    hr = getHeight r
+
+insertTree :: (Ord a) => Tree a -> a -> Tree a
+insertTree Leaf a = Node 0 Leaf a Leaf
+insertTree (Node h l n r) a
+  | a < n     = Node h (insertTree l a) n r
+  | otherwise = Node h l n (insertTree r a)
+
+genPrimes :: Integer -> [Integer]
+genPrimes n = takeWhile (<=2*n+1) . drop 1 $ primes
 
 spec :: Spec
 spec = do
@@ -29,3 +49,58 @@ spec = do
       mapM_ (liftM2 shouldBe fun2' fun2)
         [2..10]
 
+  describe "ex2" $ do
+    it "works with empty" $ do
+      foldTree ([] :: [Char]) `shouldBe` (Leaf :: Tree Char)
+
+    it "works with one item" $ do
+      foldTree "A" `shouldBe` (Node 0 Leaf 'A' Leaf)
+
+    it "works with multiple items" $ do
+      foldTree "AB" `shouldSatisfy` isBalancedTree
+      foldTree "ABCDEFG" `shouldSatisfy` isBalancedTree
+      foldTree "ABCDEFGHIJKLMNOPQRSTUVWXYZ" `shouldSatisfy` isBalancedTree
+
+  describe "ex3" $ do
+    it "xor" $ do
+      xor [] `shouldBe` False
+      xor [True] `shouldBe` True
+      xor [True, False, True] `shouldBe` False
+      xor [True, False, True, False] `shouldBe` False
+      xor [True, False, True, False, False] `shouldBe` False
+      xor [True, False, True, True, False, False] `shouldBe` True
+
+    it "map" $ do
+      mapM_ (liftM2 shouldBe (uncurry map') (uncurry map)) $
+        zip [ (+1)
+            , (*3) ]
+            [ [1..100] :: [Integer]
+            , [1..100] ]
+      mapM_ (liftM2 shouldBe (uncurry map') (uncurry map)) $
+        zip [ even
+            , odd ]
+            [ [1..100] :: [Integer]
+            , [1..200] :: [Integer] ]
+      mapM_ (liftM2 shouldBe (uncurry map') (uncurry map)) $
+        zip [ isUpper
+            , isSpace ]
+            [ "AbCDe"
+            , "A0Bcd " ]
+
+    it "foldl" $ do
+      myFoldl (flip (:)) [] "ABCD"
+        `shouldBe`
+          foldl (flip (:)) [] "ABCD"
+
+      myFoldl xor2 False [True, False, True, True, False, False]
+        `shouldBe`
+          foldl xor2 False [True, False, True, True, False, False]
+
+      myFoldl insertTree Leaf "ACBDEF"
+        `shouldBe`
+          foldl insertTree Leaf "ACBDEF"
+
+  describe "ex4" $ do
+    it "works" $ do
+      mapM_ (liftM2 shouldBe sieveSundaram genPrimes)
+        [4,10,100,1000]
